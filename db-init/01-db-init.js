a2411d85c2c259826f06944bb29b1ef99b8b6539
Jonathan (Alex) Jones <jonesjon@oregonstate.edu>
2019-06-10 21:19:57 -0700

    Create collections
    Created collections in DB init.

--- db-init/01-db-init.js
+++ db-init/01-db-init.js
@@ -1,8 +1,3 @@
-db.assignments.insertMany([
-  {
-    "courseId": "123",
-    "title": "Assignment 3",
-    "points": 100,
-    "due": "2019-06-14T17:00:00-07:00"
-  }
-]);
+db.createCollection("assignments");
+db.createCollection("courses");
+db.createCollection("users");
