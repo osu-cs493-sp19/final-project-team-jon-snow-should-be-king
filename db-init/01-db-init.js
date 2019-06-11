ac02b478c8110f7ae13a47bbc0a84c21f5de2d6c
Brett Case <brett.case97@gmail.com>
2019-06-10 18:54:24 -0700

    adding all users and auth

--- db-init/01-db-init.js
+++ db-init/01-db-init.js
@@ -6,3 +6,12 @@ db.assignments.insertMany([
     "due": "2019-06-14T17:00:00-07:00"
   }
 ]);
+
+db.users.insertMany([
+  {
+    "name": "admin",
+    "email": "admin@admin",
+    "password": "$2a$08$UhKY3YdAM9eFEQadLODgpugQ24ygiDZds1mmSfKLGTjVxclDWRyHO",
+    "role": "admin"
+  }
+]);
