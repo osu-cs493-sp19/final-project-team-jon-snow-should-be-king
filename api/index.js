29d4b78b4643531e43a7525ea9e5e6c88d3560f6
Jonathan (Alex) Jones <jonesjon@oregonstate.edu>
2019-06-12 11:54:52 -0700

    Files endpoint
    API now provides file downloading given a file ID.

--- api/index.js
+++ api/index.js
@@ -3,5 +3,6 @@ const router = require('express').Router();
 router.use('/users', require('./users'));
 router.use('/assignments', require('./assignments'));
 router.use('/courses', require('./courses'));
+router.use('/files', require('./files'));
 
 module.exports = router;
