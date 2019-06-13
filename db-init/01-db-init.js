34daf22246af0efda822d2ca2f7c1c83b473936c
Jonathan (Alex) Jones <jonesjon@oregonstate.edu>
2019-06-13 11:24:12 -0700

    Clean out dummy entries
    Cleared out dummy entries in the db creation script. Just created empty collections.

--- db-init/01-db-init.js
+++ db-init/01-db-init.js
@@ -4,37 +4,8 @@ db.users.insertMany([
     "email": "admin@admin",
     "password": "$2a$08$UhKY3YdAM9eFEQadLODgpugQ24ygiDZds1mmSfKLGTjVxclDWRyHO",
     "role": "admin"
-  },
-  {
-    "name": "Crett Base",
-    "email": "casebr@oregonstate.edu",
-    "password": "burtIsAQTPie",
-    "role": "student"
-  },
-  {
-    "name": "Mclovin",
-    "email": "mohammed@superbad.gov",
-    "password": "IAmMclovin",
-    "role": "instructor"
   }
 ]);
 
-db.courses.insertMany([
-  {
-    "subject": "CS",
-    "number": "493",
-    "title": "Cloud Application Development",
-    "term": "sp19",
-    "instructorId": "1",
-    "student_list": ["123", "234"]
-  }
-]);
-
-db.assignments.insertMany([
-  {
-    "courseId": "123",
-    "title": "Assignment 3",
-    "points": 100,
-    "due": "2019-06-14T17:00:00-07:00"
-  }
-]);
+db.createCollection("courses");
+db.createCollection("assignments");
